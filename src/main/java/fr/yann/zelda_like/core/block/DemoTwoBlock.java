8111563cbd48a5fc09280bb8cd8748291bc4cbf4
Neutron-Pro <neutron@neutronstars.fr>
2022-06-11 19:57:47 +0200

    Ajout de la texture des mur demo

--- src/main/java/fr/yann/zelda_like/core/block/DemoTwoBlock.java
+++ src/main/java/fr/yann/zelda_like/core/block/DemoTwoBlock.java
@@ -2,10 +2,18 @@ package fr.yann.zelda_like.core.block;
 
 import fr.yann.zelda_like.api.ZeldaLike;
 import fr.yann.zelda_like.api.level.Location;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 
 public class DemoTwoBlock extends AbstractBlock {
+    private static final Image texture = new Image("assets/textures/wall.png");
+
     public DemoTwoBlock(ZeldaLike zeldaLike, Location location) {
         super(zeldaLike, location, Color.color(0, 0.5, 0.5), false);
+    }
+
+    @Override
+    public Image getTexture() {
+        return DemoTwoBlock.texture;
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module fr.yann.zelda_like {
     requires javafx.controls;
 
-
+    opens assets.textures;
     exports fr.yann.zelda_like;
 }
